520e960c3f82482d082c0be6ce0bcc4942563189
DRpandaMD <michael.zarate.ce@gmail.com>
2017-11-07 16:37:19 -0600

    added a PostgreSQL DB backend for POST and GET

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,26 @@
-# "Database code" for the DB Forum.
+# Michael Zarate started here:
 
-import datetime
+# Changing code to implement a PostgreSQL DB
+import psycopg2
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
+DBNAME = "forum"
+
 
 def get_posts():
-  """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+    """Return all posts from the 'database', most recent first."""
+    dbconnection = psycopg2.connect(database=DBNAME)
+    dbcursor = dbconnection.cursor()
+    dbcursor.execute("select content, time from posts order by time desc")
+    return dbcursor.fetchall()
+    dbconnection.close()
+
 
 def add_post(content):
-  """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
+    """Add a post to the 'database' with the current timestamp."""
+    dbconnection = psycopg2.connect(database=DBNAME)
+    dbcursor = dbconnection.cursor()
+    dbcursor.execute("insert into posts values ('%s')" % content)
+    dbconnection.commit()
+    dbconnection.close()
 
 
