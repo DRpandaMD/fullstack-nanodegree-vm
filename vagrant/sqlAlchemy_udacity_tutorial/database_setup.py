a89976cdd475de1ba13cfc8874d2d07b1d1d6e4b
DRpandaMD <michael.zarate.ce@gmail.com>
2017-12-12 13:54:56 -0600

    in /sqlAlchemy_udacity_tutorial : successfully created 'database_setup.py' which gave me a db 'restaurantmenu.db'

--- vagrant/sqlAlchemy_udacity_tutorial/database_setup.py
+++ vagrant/sqlAlchemy_udacity_tutorial/database_setup.py
@@ -3,4 +3,35 @@
 # Michael Zarate 12/11/2017
 
 import sys
-from sqlalchemy
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+
+# Lets sqlalchemy know these are special classes
+Base = declarative_base()
+
+
+# now we are going to declare our two classes for the two tables we are going to use in the db
+class Restaurant(Base):
+    __tablename__ = "restaurant"
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+
+class MenuItem(Base):
+    __tablename__ = "menu_item"
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey("restaurant.id"))
+    restaurant = relationship(Restaurant)
+
+
+# insert at EOF #
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
+
